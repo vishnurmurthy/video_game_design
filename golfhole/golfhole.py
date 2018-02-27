04b3052897f409cdb4595a61985bd229ae4db11c
BedrockSolid <bedrocksolidmc@gmail.com>
2018-02-26 19:13:22 -0500

    added to golf minigame
    Randomly chooses a percentage and stops w/ spacebar press, hole has random distance from ball (which isn't there)
    No graphical way to prove this but most game logic done

--- golfhole/golfhole.py
+++ golfhole/golfhole.py
@@ -1,13 +1,14 @@
 # this is a decent template for a minigame, if you want to use it (and we probably should use something that's the same)
 
 import pygame, os
+from random import randint
 from pygame.locals import *
 
 class Game:
     def __init__(self):
         self._running = True
         self._display_sfc = None
-        self.size = self.weight, self.height = 640, 480
+        self.size = self.width, self.height = 640, 480
  
     def init(self): 
         pygame.init()
@@ -24,7 +25,16 @@ class Game:
         pygame.mixer.music.play()
         
         pygame.display.set_caption("TSA Video Game Design")
-        self._display_sfc.fill([255, 255, 255])
+        self._display_sfc.fill([128, 255, 128])
+        
+        pygame.font.init()
+        self.on = 0
+        self.gauge = 1
+        self.direct = 0.1
+        self.myfont = pygame.font.SysFont('Arial', 30)
+        self.gtext = self.myfont.render(str(self.gauge), False, (0, 0, 0))
+        
+        self.random = randint(25,75)
  
     def events(self, event):
         if event.type == pygame.QUIT:
@@ -33,6 +43,16 @@ class Game:
     def loop(self):
         if not pygame.mixer.music.get_busy():
             self._running = False
+        if self.gauge >= 100 or self.gauge <= 0:
+            self.direct *= -1
+        if pygame.key.get_pressed()[K_SPACE]:
+            self.on = 1
+        if not self.on:
+            self.gauge += self.direct
+        self.gtext = self.myfont.render(str(int(self.gauge)), False, (0, 0, 0))
+        self._display_sfc.fill([128, 255, 128])
+        pygame.draw.circle(self._display_sfc, (0, 0, 0), (self.width/2,self.height/2-self.random*2), 15)
+        self._display_sfc.blit(self.gtext,(0,0))
         
     def render(self):
         pygame.display.flip()
